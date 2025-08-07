0eca9f77e3ce0058a387ad5cb6abf19a5cd121c2
Patricia Souza <patricia.sz.br@gmail.com>
2025-08-07 07:08:07 -0300

    tipo de dados

---
+++ 01_fundamentals/2-tipos_de_dados.py
@@ -0,0 +1,13 @@
+#PythonFlix
+
+name = "Top Gun Maverik"
+
+year_lauch = 2023
+
+noteMovie = 9.5
+planInclude = False
+
+print(type(name))
+print(type(year_lauch))
+print(type(noteMovie))
+print(type(planInclude))
