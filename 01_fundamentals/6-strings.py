4c3500bbffaa7d2d9252ed35f6534152d3b686ad
Patricia Souza <patricia.sz.br@gmail.com>
2025-08-09 12:09:55 -0300

    utilizando strings

---
+++ 01_fundamentals/6-strings.py
@@ -0,0 +1,23 @@
+movieName = "Top Gun"
+movieName2 = "top Gun"
+
+print(movieName == movieName2) # Case sensitive
+
+movieDescription = """
+  Top Gun é um filme de aviação e aventura
+muito consagrado na industria
+"""
+
+
+print(movieName)
+
+# 1 - Multiplicação de strings
+
+line = "="
+print(line*50)
+print(movieDescription)
+
+# 2 - Procurar uma palavra dentro de um texto
+
+print("top" in movieName)
+print("ação" in movieName)
